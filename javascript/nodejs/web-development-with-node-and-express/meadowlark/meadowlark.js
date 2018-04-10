eeff33cd840b2240d7658b40a34780abe4a4df4c
iamlow <iamlow@gmail.com>
2018-04-10 21:08:40 +0900

    Update fortune.js

--- javascript/nodejs/web-development-with-node-and-express/meadowlark/meadowlark.js
+++ javascript/nodejs/web-development-with-node-and-express/meadowlark/meadowlark.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var fortune = require('./lib/fortune.js');
 var app = express();
 app.set('port', process.env.PORT || 3000);
 
@@ -14,9 +15,7 @@ app.get('/', function(req, res) {
 });
 
 app.get('/about', function(req, res) {
-  var randomFortune =
-      fortunes[Math.floor(Math.random() * fortunes.length)];
-  res.render('about', { fortune: randomFortune });
+  res.render('about', { fortune: fortune.getFortune() });
 });
 
 app.use(function(req, res, next) {
@@ -34,11 +33,3 @@ app.listen(app.get('port'), function() {
   console.log('Express started on http://localhost:' +
       app.get('port') + '; press Ctrl-C to terminate.');
 });
-
-var fortunes = [
-  "Conquer your fears or they will conquer you.",
-  "Rivers need springs.",
-  "Do not fear what you don't know.",
-  "You will have a pleasant surprise.",
-  "Whenever possible, keep it simple.",
-];
