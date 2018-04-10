c2c5f764cbd35afc00faf0a118bed079bc28c998
iamlow <iamlow@gmail.com>
2018-04-10 20:16:47 +0900

    Add handlers for express.js

--- javascript/nodejs/meadowlark/site/meadowlark.js
+++ javascript/nodejs/meadowlark/site/meadowlark.js
@@ -1,31 +1,29 @@
 var express = require('express');
 var app = express();
-
-const handlebars = require('express-handlebars').create({default:'main'});
-app.engine('handlebars', handlebars.engine);
 app.set('port', process.env.PORT || 3000);
 
+var handlebars = require('express-handlebars')
+    .create({ defaultLayout:'main' });
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
 app.get('/', function(req, res) {
-  res.type('text/plain');
-  res.send('Meadowlark Travel');
+  res.render('home');
 });
 
 app.get('/about', function(req, res) {
-  res.type('text/plain');
-  res.send('About Meadowlark Travel');
+  res.render('about');
 });
 
-app.use(function(req, res) {
-  res.type('text/plain');
+app.use(function(req, res, next) {
   res.status(400);
-  res.send('404 - Not Found');
+  res.render('404');
 });
 
 app.use(function(err, req, res, next) {
   console.error(err.stack);
-  res.type('text/plain');
   res.status(500);
-  res.send('500 - Server Error');
+  res.render('500');
 });
 
 app.listen(app.get('port'), function() {
