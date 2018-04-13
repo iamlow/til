f8cdc7b28100bc4259fcfccf0388b4158d7381ff
iamlow <iamlow@gmail.com>
2018-04-13 15:55:59 +0900

    Update chapter 8

--- javascript/nodejs/web-development-with-node-and-express/meadowlark/meadowlark.js
+++ javascript/nodejs/web-development-with-node-and-express/meadowlark/meadowlark.js
@@ -1,10 +1,20 @@
 var express = require('express');
 var fortune = require('./lib/fortune.js');
+var formidable = require('formidable');
+
 var app = express();
 app.set('port', process.env.PORT || 3000);
 
-var handlebars = require('express-handlebars')
-    .create({ defaultLayout:'main' });
+var handlebars = require('express-handlebars').create({
+  defaultLayout:'main',
+  helpers: {
+    section: function(name, options) {
+      if (!this._sections) this._sections = {};
+      this._sections[name] = options.fn(this);
+      return null;
+    }
+  }
+});
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
@@ -13,6 +23,63 @@ app.use(express.static(__dirname + '/public'));
 app.use(function(req, res, next) {
   res.locals.showTests = app.get('env') !== 'production' &&
       req.query.test === '1';
+  next();
+});
+
+app.use(require('body-parser').urlencoded({ extended: true}));
+
+app.get('/thank-you', function(req, res){
+	res.render('thank-you');
+});
+
+app.get('/newsletter', function(req, res) {
+  res.render('newsletter', { csrf: 'CSRF token goes here' });
+});
+
+app.post('/process', function(req, res) {
+  // console.log('Form (from querystring): ' + req.query.form);
+  // console.log('CSRF token (from hidden form field): ' + req.body._csrf);
+  // console.log('Name (from visible form field): ' + req.body.name);
+  // console.log('Email (from visible form field): ' + req.body.email);
+  // res.redirect(303, '/thank-you');
+  if (req.xhr || req.accepts('json,html') === 'json') {
+    res.send({success: true});
+  } else {
+    res.redirect(303, '/thank-you');
+  }
+});
+
+function getWeatherData() {
+  return {
+    locations: [
+      {
+        name: 'Portland',
+        forecastUrl: 'http://www.wunderground.com/US/OR/Portland.html',
+        iconUrl: 'http://icons-ak.wxug.com/i/c/k/cloudy.gif',
+        weather: 'Overcast',
+        temp: '54.1 F (12.3 C)',
+      },
+      {
+        name: 'Bend',
+        forecastUrl: 'http://www.wunderground.com/US/OR/Bend.html',
+        iconUrl: 'http://icons-ak.wxug.com/i/c/k/partlycloudy.gif',
+        weather: 'Partly Cloudy',
+        temp: '55.0 F (12.8 C)',
+      },
+      {
+        name: 'Manzanita',
+        forecastUrl: 'http://www.wunderground.com/US/OR/Manzanita.html',
+        iconUrl: 'http://icons-ak.wxug.com/i/c/k/rain.gif',
+        weather: 'Light Rain',
+        temp: '55.0 F (12.8 C)',
+      },
+    ],
+  };
+}
+
+app.use(function(req, res, next) {
+  if(!res.locals.partials) res.locals.partials = {};
+  res.locals.partials.weatherContext = getWeatherData();
   next();
 });
 
@@ -37,6 +104,40 @@ app.get('/tours/request-group-rate', function(req, res) {
 
 app.get('/tours/oregon-coast', function(req, res) {
   res.render('tours/oregon-coast');
+});
+
+app.get('/jquery-test', function(req, res){
+	res.render('jquery-test');
+});
+
+app.get('/nursery-rhyme', function(req, res){
+	res.render('nursery-rhyme');
+});
+
+app.get('/data/nursery-rhyme', function(req, res){
+	res.json({
+		animal: 'squirrel',
+		bodyPart: 'tail',
+		adjective: 'bushy',
+		noun: 'heck',
+	});
+});
+
+app.get('/contest/vacation-photo', function(req, res){
+    var now = new Date();
+    res.render('contest/vacation-photo', { year: now.getFullYear(), month: now.getMonth() });
+});
+
+app.post('/contest/vacation-photo/:year/:month', function(req, res){
+    var form = new formidable.IncomingForm();
+    form.parse(req, function(err, fields, files){
+        if(err) return res.redirect(303, '/error');
+        console.log('received fields:');
+        console.log(fields);
+        console.log('received files:');
+        console.log(files);
+        res.redirect(303, '/thank-you');
+    });
 });
 
 app.use(function(req, res, next) {
